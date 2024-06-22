2ebf22b68faf282db20b517e891dfdf2e1f8924c
JohaenNiro <johaengnanakumar160@gmail.com>
2024-06-21 21:28:48 -0400

    first commit

---
+++ .history/index_20240621174814.js
@@ -0,0 +1,10 @@
+<!DOCTYPE; html>
+<html lang="en">
+<head>
+<meta> charset="utf-8" </meta>
+<title>Error</title>
+</head>
+<body>
+<pre>hello</pre>
+</body>
+</html>
---
+++ .history/server_20240621175635.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+const productRoutes = require('./routes/productRoutes');
+const connectDB = require('./config/db');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+app.get('/', (req, res) => res.send('API is running...'));
+
+// Routes
+app.use('/api/products', productRoutes);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
